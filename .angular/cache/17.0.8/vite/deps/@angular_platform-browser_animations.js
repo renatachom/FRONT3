fd646b294f51ff6b7ae280e65c980f4640e2a711
renatachom <renatachom.ads@gmail.com>
2024-02-07 18:23:31 -0300

    Enviando Arquivos

---
+++ .angular/cache/17.0.8/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,23 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-KLBJT6AJ.js";
+import "./chunk-HW7L2IGY.js";
+import "./chunk-JNA4PZRX.js";
+import "./chunk-I7F2S3EW.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-HXK7VK5Z.js";
+import "./chunk-G25BR4HC.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
